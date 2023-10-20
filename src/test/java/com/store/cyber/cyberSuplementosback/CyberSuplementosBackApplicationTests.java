557809ace63e0495030be4e1db878ef4c7666ab4
Lucas Porfírio <34553105+Porfirio1997@users.noreply.github.com>
2023-10-20 13:35:52 -0300

    Initial commit - Santander Dev Week 2023

---
+++ src/test/java/com/store/cyber/cyberSuplementosback/CyberSuplementosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.cyber.cyberSuplementosback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberSuplementosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
